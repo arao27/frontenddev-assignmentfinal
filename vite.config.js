4eee709a8d295d10e40d6d581953bf5a3af361a9
Tony Rao <arao27@adrian.edu>
2026-02-25 01:02:04 -0500

    Update files for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ensures relative paths in Vercel
-})
+  root: './',           // project root
+  base: './',           // relative paths
+  publicDir: 'public',  // static assets
+  build: {
+    outDir: 'public/assets',   // compiled JS will go into public/assets
+    rollupOptions: {
+      input: resolve(__dirname, 'public/index.html'),
+    },
+  },
+});
