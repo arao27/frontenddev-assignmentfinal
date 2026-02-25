c9314759a0e27eead9a6b4373bd7f28aa4340233
Tony Rao <arao27@adrian.edu>
2026-02-25 01:19:26 -0500

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: './',           // project root
-  base: './',           // relative paths
-  publicDir: 'public',  // static assets
+  root: './',           
+  base: './',           
+  publicDir: 'public',  
   build: {
-    outDir: 'public/assets',   // compiled JS will go into public/assets
-    rollupOptions: {
-      input: resolve(__dirname, 'public/index.html'),
-    },
+    outDir: 'public/assets', // compiled JS goes here
   },
 });
